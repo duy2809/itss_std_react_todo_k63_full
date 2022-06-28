5b57c115d2308e40cecac9a89722ef10bdb066ef
Đỗ Duy Anh <duyanhvn1999@gmail.com>
2022-06-28 17:13:37 +0700

    Login by Authentication

--- src/App.js
+++ src/App.js
@@ -1,15 +1,59 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import Login from "./components/Login";
+import { auth, storeUserInfo } from "./lib/firebase";
 
 /* スタイルシート */
-import './styles/main.css';
+import "./styles/main.css";
 
 /* コンポーネント */
-import Todo from './components/Todo';
+import Todo from "./components/Todo";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState([]);
+
+  useEffect(() => {
+    auth.onAuthStateChanged(async (user) => {
+      setLoading(false);
+      let newUser = null;
+      if (user) {
+        newUser = await storeUserInfo(user);
+      }
+      setUser(newUser);
+    });
+  }, []);
+
+  const logout = () => {
+    auth.signOut();
+  };
+
+  const HeaderContent = () => {
+    if (user) {
+      return (
+        <div className="navbar-end">
+          <div className="navbar-item">{user.name}</div>
+          <div className="navbar-item">
+            <button
+              className="button is-danger is-light is-small"
+              onClick={logout}
+            >
+              {" "}
+              Logout
+            </button>
+          </div>
+        </div>
+      );
+    } else {
+      return <Login />;
+    }
+  };
+
   return (
     <div className="container is-fluid">
-      <Todo />
+      <header className="navbar">
+        {loading ? <p>LOADING.....</p> : <HeaderContent />}
+      </header>
+      <div>{user && <Todo />}</div>
     </div>
   );
 }
--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,6 +1,6 @@
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/auth';
-import 'firebase/compat/firestore';
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBzMVolHVXS9j4ecPlQTdCWTaP7A8uWhvY",
@@ -8,7 +8,7 @@ const firebaseConfig = {
   projectId: "itss-k63",
   storageBucket: "itss-k63.appspot.com",
   messagingSenderId: "497670202578",
-  appId: "1:497670202578:web:2b7a5dd5b680d04bedb90b"
+  appId: "1:497670202578:web:2b7a5dd5b680d04bedb90b",
 };
 
 firebase.initializeApp(firebaseConfig);
@@ -54,4 +54,27 @@ export const clearFirebaseItem = async (item) => {
     .catch(function (err) {
       console.log(err);
     });
+};
+
+export const uiConfig = {
+  signInFlow: "popup",
+  signInSuccessUrl: "/",
+  signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID],
+};
+
+export const storeUserInfo = async (user) => {
+  const { uid } = user;
+  const userDoc = await db.collection("users").doc(uid).get();
+  if (!userDoc.exists) {
+    await db.collection("users").doc(uid).set({ name: user.displayName });
+    return {
+      name: user.displayName,
+      id: uid,
+    };
+  } else {
+    return {
+      id: uid,
+      ...userDoc.data(),
+    };
+  }
 };
